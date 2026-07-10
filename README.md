19bcc5b05722e2b09d2fdc6afe6460fe21434756
1vivy <1vivy@tutanota.com>
2026-07-10 01:24:12 +0000

    camera-macanc: regenerate from current v3 extraction
    Change-Id: Iac4a2a24c68d60bcd49474ca1b77b919b64802e9

--- README.md
+++
@@ -1,9 +0,0 @@
-# proprietary_vendor_oneplus_camera-macanc
-
-Blob-only proprietary camera repo scaffold for OnePlus Ace 6T (macanc).
-
-This repository is reserved for device-specific proprietary camera payloads and
-currently contains only the LFS metadata and scaffold until payload extraction is
-committed.
-
-- Build path: `proprietary/vendor/oneplus/camera-macanc`
---
+++ camera-macanc-vendor.mk
@@ -0,0 +1,52 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    proprietary/vendor/oneplus/camera-macanc
+
+PRODUCT_COPY_FILES += \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/camera_engmode.xml:$(TARGET_COPY_OUT_ODM)/etc/camera/camera_engmode.xml \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/cfr_bin/CFR_macanMain.bin:$(TARGET_COPY_OUT_ODM)/etc/camera/cfr_bin/CFR_macanMain.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/cfr_bin/CFR_macanWide.bin:$(TARGET_COPY_OUT_ODM)/etc/camera/cfr_bin/CFR_macanWide.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/config/camera_third_party_feature_config.protobuf:$(TARGET_COPY_OUT_ODM)/etc/camera/config/camera_third_party_feature_config.protobuf \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/config/camera_unit_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/camera_unit_config \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/config/camera_unit_feature_config.protobuf:$(TARGET_COPY_OUT_ODM)/etc/camera/config/camera_unit_feature_config.protobuf \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/config/macanFront:$(TARGET_COPY_OUT_ODM)/etc/camera/config/macanFront \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/config/macanMain:$(TARGET_COPY_OUT_ODM)/etc/camera/config/macanMain \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/config/macanWide:$(TARGET_COPY_OUT_ODM)/etc/camera/config/macanWide \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/config/oplus_camera_algo_switch_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_algo_switch_config \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/config/oplus_camera_aps_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_aps_config \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/config/oplus_camera_config:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_config \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/config/oplus_camera_preview_decision_config.json:$(TARGET_COPY_OUT_ODM)/etc/camera/config/oplus_camera_preview_decision_config.json \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/dng/ColorMatrix1_macanMain.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/dng/ColorMatrix1_macanMain.txt \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/dng/ColorMatrix1_macanWide.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/dng/ColorMatrix1_macanWide.txt \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/dng/ColorMatrix2_macanMain.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/dng/ColorMatrix2_macanMain.txt \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/dng/ColorMatrix2_macanWide.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/dng/ColorMatrix2_macanWide.txt \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/dng/D28HUESATMap_macanMain.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/dng/D28HUESATMap_macanMain.txt \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/dng/D28HUESATMap_macanWide.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/dng/D28HUESATMap_macanWide.txt \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/dng/D65HUESATMap_macanMain.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/dng/D65HUESATMap_macanMain.txt \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/dng/D65HUESATMap_macanWide.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/dng/D65HUESATMap_macanWide.txt \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/dng/ToneCurve_macanMain.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/dng/ToneCurve_macanMain.txt \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/dng/ToneCurve_macanWide.txt:$(TARGET_COPY_OUT_ODM)/etc/camera/dng/ToneCurve_macanWide.txt \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/eis_camera.vcfg:$(TARGET_COPY_OUT_ODM)/etc/camera/eis_camera.vcfg \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/facerestore/InitParams.json:$(TARGET_COPY_OUT_ODM)/etc/camera/facerestore/InitParams.json \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/fwk_config.json:$(TARGET_COPY_OUT_ODM)/etc/camera/fwk_config.json \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/oplus_eis_camera.vcfg:$(TARGET_COPY_OUT_ODM)/etc/camera/oplus_eis_camera.vcfg \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/etc/camera/vainr_cfg.json:$(TARGET_COPY_OUT_ODM)/etc/camera/vainr_cfg.json \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/awb_parameter_macanFront.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/awb_parameter_macanFront.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/awb_parameter_macanMain.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/awb_parameter_macanMain.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/awb_parameter_macanWide.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/awb_parameter_macanWide.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/com.qti.sensormodule.macanFront.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.sensormodule.macanFront.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/com.qti.sensormodule.macanMain.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.sensormodule.macanMain.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/com.qti.sensormodule.macanWide.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.sensormodule.macanWide.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/com.qti.tuned.molokai_macanFront.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.tuned.molokai_macanFront.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/com.qti.tuned.molokai_macanMain.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.tuned.molokai_macanMain.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/com.qti.tuned.molokai_macanWide.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/com.qti.tuned.molokai_macanWide.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/libaecCustomParam_Cam0.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/libaecCustomParam_Cam0.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/libaecCustomParam_Cam1.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/libaecCustomParam_Cam1.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/libaecCustomParam_Cam2.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/libaecCustomParam_Cam2.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/libafCustomParam_Cam0.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/libafCustomParam_Cam0.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/libpdCustomParam_Cam0.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/libpdCustomParam_Cam0.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/libpdCustomParam_Cam2.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/libpdCustomParam_Cam2.bin \
+    proprietary/vendor/oneplus/camera-macanc/proprietary/odm/lib64/camera/libpdCustomParam_Cam3.bin:$(TARGET_COPY_OUT_ODM)/lib64/camera/libpdCustomParam_Cam3.bin
